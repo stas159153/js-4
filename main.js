8bf9b1996c55b5c8b6dba8733740bbc8a7451df5
Staas <zdanie159153@gmail.com>
2023-02-21 21:02:32 +0200

    commit

---
+++ main.js
@@ -0,0 +1,23 @@
+// for (let a = 0 ; a<=10; a++ ) {
+//   let b = prompt("enter your number ");
+//   if ( b == 10 ){
+//     alert ( "Ok" )
+//   }
+//   else {
+//     alert( "error" )
+//   }
+
+// }
+for (let a = 4; a <= 15; a++) {
+  console.log(a);
+}
+for (let b = 0; b < 8; b++) {
+  console.log(8);
+}
+let v = prompt("Enter your number");
+for (let c = 0; c < v; c++) {
+  console.log("привіт");
+}
+
+
+
